9d835273ffa42d60758636c5f74f77ee8918b202
Marwaya <marwa_badawi_claes@yahoo.com>
2018-09-01 09:17:29 -0800

    Test

--- local-weather-app/src/app/app.module.ts
+++ local-weather-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CurrentweatherComponent } from './currentweather/currentweather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentweatherComponent
   ],
   imports: [
     BrowserModule
---
+++ local-weather-app/src/app/currentweather/currentweather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currentweather',
+  templateUrl: './currentweather.component.html',
+  styleUrls: ['./currentweather.component.css']
+})
+export class CurrentweatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
